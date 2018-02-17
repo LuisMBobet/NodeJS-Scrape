7094e9bcd77da682f8c803d960fa7c3d030bcf32
LuisMBobet <30813823+LuisMBobet@users.noreply.github.com>
2018-02-17 16:30:39 +0000

    Finished main.js
    Finished main.js so that it can be called from command line using 1 string argument in the form of an email address.

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 var InputProcess = require("./local_modules/inputProcess.js");
-var OutputProcess = require("./local_modules/outputProcess.js");
 var Scrape = require("./local_modules/scrape.js");
+
+
+//Call from command line using 1 argument. For best results use a valid email address.
+
+if (process.argv.length === 3){
+  var domain = InputProcess(process.argv[2]);
+  if(domain) Scrape(domain);
+}else{
+  console.log("Error : Incorrect number of arguments");
+}
